55767a4d190022e24f74e9847c6a0d940c2146e2
Your Name <your.email@example.com>
2025-03-24 01:52:11 +0530

    asdf

--- Python/OTP_VERIFICATION.py
+++ Python/OTP_VERIFICATION.py
@@ -8,8 +8,9 @@ digits="0123456789"
 OTP=""
 #Six digit password
 for i in range(6):
-    OTP+=digits[math.floor(random.random()*10)]
+    OTP+=digits[math.floor(random.random()*6)]
 otp = OTP + " is your OTP"
 msg= otp
 
 print(msg)
+
